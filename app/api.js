d251f49b3e25c166ad22b3997c4a0126ead4b21e
Aaron Sky <aaronsky@skyaaron.com>
2016-02-04 14:14:11 -0500

    Removed unused test endpoint

--- app/api.js
+++ app/api.js
@@ -10,23 +10,6 @@ app.get('/', function(req, res) {
   res.send('Root currently serves no function');
 });
 
-app.get('/users/:id', function(req, res) {
-  var id = req.params.id;
-  console.log('Requested user ' + id);
-  var i, len;
-  for (i = 0, len = db_fake.length; i < len; i++) {
-    var record = db_fake[i];
-    console.log(record);
-    if (record.id == id) {
-      console.log('match found');
-      res.send(record.name + ' : ' + record.email);
-      return;
-    }
-  }
-  console.log('not found');
-  res.send('Not found');
-});
-
 app.get('/repos', function(req, res) {
   res.send(Configuration.repos);
 });
