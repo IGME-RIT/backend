07d4e4461aecebec2ae7ccdcec777bbf9bea9c12
Aaron Sky <aaronsky@skyaaron.com>
2016-02-18 12:54:53 -0500

    Revised units until MongoDB migration is complete

--- tests/test_author.js
+++ tests/test_author.js
@@ -1,17 +1,17 @@
 var expect = require('chai').expect;
 
-var Author = require('../app/models/author.js');
+var Author = require('../app/models/author.js').Author;
 
 describe('Author', function () {
   context('#constructor', function () {
     it('should set default property values with no arguments', function () {
-      var author = new Author();
+      var author = new Author({});
       expect(author).to.have.deep.property('name', 'Unknown author');
       expect(author).to.have.deep.property('email', 'bad@email.com');
       expect(author).to.have.deep.property('github', 'Unknown GitHub name');
     });
     it('should set the right properties when passed as arguments', function () {
-      var author = new Author('Aaron Sky', 'aaronsky@skyaaron.com', 'aaronsky');
+      var author = new Author({name:'Aaron Sky', email:'aaronsky@skyaaron.com', github:'aaronsky'});
       expect(author).to.have.deep.property('name', 'Aaron Sky');
       expect(author).to.have.deep.property('email', 'aaronsky@skyaaron.com');
       expect(author).to.have.deep.property('github', 'aaronsky');
