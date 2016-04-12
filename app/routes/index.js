6e6cf263e4499d89f9967ae90cf50870168d7a27
Aaron Sky <aaronsky@skyaaron.com>
2016-04-12 11:55:23 -0400

    Enabled CORS on root endpoint

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,16 @@
+var cors = require('cors');
+
 var middleware = require('../middleware');
 var syncRoutes = require('./sync');
 
 module.exports = function(repos) {
     var router = function(app) {
         var repoRoutes = require('./repos')(repos);
-
         app.use('/repos', repoRoutes);
         app.use('/sync', syncRoutes);
+        app.options('/', cors());
         app.get('/',
+            cors(),
             middleware.requiresSecure,
             function(req, res) {
                 res.json({
